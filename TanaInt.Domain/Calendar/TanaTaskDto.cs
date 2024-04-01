7a4335f3169df1ed1e1d3d78511d5b7d7f6f6b94
popemkt <hoangng71299@gmail.com>
2024-04-01 22:19:55 +0700

    Update parsing logic following bug fix

--- TanaInt.Domain/Calendar/TanaTaskDto.cs
+++ TanaInt.Domain/Calendar/TanaTaskDto.cs
@@ -8,14 +8,14 @@ public class TanaTaskDto
 {
     [JsonPropertyName("name")] public string Name { get; set; }
     [JsonPropertyName("url")] public string Url { get; set; }
-    [JsonPropertyName("context")] public string Context { get; set; }
     [JsonPropertyName("refString")] public string RefString { get; set; }
+    [JsonPropertyName("date")] public string Date { get; set; }
     [JsonPropertyName("id")] public string? GCalEventId { get; set; }
+    [JsonPropertyName("doneTime")] public string DoneTime { get; set; }
+    [JsonPropertyName("scheduled")] public string Scheduled { get; set; }
     [JsonIgnore] public DateTime Start { get; set; }
     [JsonIgnore] public DateTime End { get; set; }
     [JsonIgnore] public bool IsAllDay { get; set; }
-    [JsonPropertyName("doneTime")] public string DoneTime { get; set; }
-    [JsonPropertyName("scheduled")] public string Scheduled { get; set; }
 
     public string FormatName()
     {
@@ -26,28 +26,16 @@ public class TanaTaskDto
 
     public TanaTaskDto ParseInput()
     {
-        var lines = Context.Split("\n");
-        (IsAllDay, Start, End) = ParseDates(lines.Last(l => l.StartsWith("  - Date::")));
-        GCalEventId = string.IsNullOrWhiteSpace(RefString) ? null : ParseRefString(RefString);
-        DoneTime = ParseDoneTime(lines.FirstOrDefault()?.Substring(0, 5));
+        (IsAllDay, Start, End) = ParseDateToIntervals(Date);
+        GCalEventId = string.IsNullOrWhiteSpace(RefString) ? null : ParseGCalRefString(RefString);
         return this;
     }
 
-    private string ParseDoneTime(string? status)
+    private string ParseGCalRefString(string refString) => refString.Split(", ")[1];
+
+    private (bool IsAllDay, DateTime Start, DateTime End) ParseDateToIntervals(string dateString)
     {
-        if (string.IsNullOrWhiteSpace(status)) return string.Empty;
-
-        if (status.Contains("x", StringComparison.InvariantCultureIgnoreCase))
-            return status;
-
-        return string.Empty;
-    }
-
-    private string ParseRefString(string refString) => refString.Split(", ")[1];
-
-    private (bool IsAllDay, DateTime Start, DateTime End) ParseDates(string line)
-    {
-        var dateStrings = Utils.ExtractDateStrings(line);
+        var dateStrings = Utils.ExtractDateStrings(dateString);
         var start = DateTime.Parse(dateStrings[0], CultureInfo.InvariantCulture);
         var isAllDay = (start - start.Date).TotalSeconds == 0 && dateStrings.Length == 1;
         var end = dateStrings.Length > 1
