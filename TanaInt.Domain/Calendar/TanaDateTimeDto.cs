7f8c35238d2cfbd5d8df78c910a93d997d267e10
popemkt <hoangng71299@gmail.com>
2024-04-01 23:09:18 +0700

    Adjust following tana bugfix

--- TanaInt.Domain/Calendar/TanaDateTimeDto.cs
+++ TanaInt.Domain/Calendar/TanaDateTimeDto.cs
@@ -1,27 +1,23 @@
 using System.Globalization;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 
 namespace TanaInt.Domain.Calendar;
 
 public class TanaDateTimeDto
 {
     [JsonPropertyName("rrule")] public string RRule { get; set; }
-    [JsonPropertyName("context")] public string Context { get; set; }
+    [JsonPropertyName("date")] public string Date { get; set; }
     [JsonIgnore] public DateTime OccurenceDate { get; set; }
 
     public TanaDateTimeDto ParseInput()
     {
-        var lines = Context.Split("\n");
-        
-        OccurenceDate = ParseDate(lines.First(l => l.StartsWith("  - Date::")));
-
+        OccurenceDate = ParseDate(Date);
         return this;
     }
 
-    private DateTime ParseDate(string line)
+    private DateTime ParseDate(string dateString)
     {
-        var dates = Utils.ExtractDateStrings(line);
+        var dates = Utils.ExtractDateStrings(dateString);
         return DateTime.Parse(dates[0], CultureInfo.InvariantCulture);
     }
 }
