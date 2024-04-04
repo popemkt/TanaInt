40c3588b239fff9b0725e23db1124d885b33fe5b
Hoang Nguyen Gia <hoang.nguyen.gia@topicus.nl>
2024-04-04 14:07:07 +0700

    Added test for Fsrs

---
+++ TanaInt.Domain.Tests/FSRSTests.cs
@@ -0,0 +1,134 @@
+using TanaInt.Domain.Srs.Fsrs;
+
+namespace TanaInt.Domain.Tests;
+
+using System;
+using Xunit;
+
+public class FSRSTests
+{
+    [Fact]
+    public void Rating_Again_ShouldEqual1()
+    {
+        Assert.Equal(1, (int)Rating.Again);
+    }
+
+    [Fact]
+    public void State_New_ShouldEqual0()
+    {
+        Assert.Equal(0, (int)State.New);
+    }
+
+    [Fact]
+    public void Card_State_ShouldEqual0()
+    {
+        var card = new Card();
+        Assert.Equal(0, (int)card.State);
+    }
+
+    [Fact]
+    public void FSRS_P_W_ShouldMatchExpectedValues()
+    {
+        var fsrs = new FSRS();
+        var expectedW = new double[]
+        {
+            0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05,
+            0.34, 1.26, 0.29, 2.61
+        };
+        Assert.Equal(expectedW, fsrs.P.W);
+    }
+
+    [Fact]
+    public void TestRepeat()
+    {
+        var f = new FSRS();
+        var cardTest = new Card();
+        f.P.W = new double[]
+        {
+            1.14, 1.01, 5.44, 14.67, 5.3024, 1.5662, 1.2503, 0.0028, 1.5489, 0.1763,
+            0.9953, 2.7473, 0.0179, 0.3105, 0.3976, 0.0, 2.0902
+        };
+
+        var now = new DateTime(2022, 11, 29, 12, 30, 0, 0);
+        var schedulingCards = f.Repeat(cardTest, now);
+
+        var ratings = new Rating[]
+        {
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Again,
+            Rating.Again,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good
+        };
+        var ivlHistory = new int[ratings.Length];
+        var stateHistory = new State[ratings.Length];
+
+        for (int i = 0; i < ratings.Length; i++)
+        {
+            cardTest = schedulingCards[ratings[i]].Card;
+            ivlHistory[i] = cardTest.ScheduledDays;
+            stateHistory[i] = schedulingCards[ratings[i]].ReviewLog.State;
+            now = cardTest.Due;
+            schedulingCards = f.Repeat(cardTest, now);
+        }
+
+        var expectedIvlHistory = new int[]
+        {
+            0, 5, 16, 43, 106, 236, 0, 0, 12, 25, 47, 85, 147
+        };
+        var expectedStateHistory = new State[]
+        {
+            State.New,
+            State.Learning,
+            State.Review,
+            State.Review,
+            State.Review,
+            State.Review,
+            State.Review,
+            State.Relearning,
+            State.Relearning,
+            State.Review,
+            State.Review,
+            State.Review,
+            State.Review
+        };
+
+        Assert.Equal(expectedIvlHistory, ivlHistory);
+        Assert.Equal(expectedStateHistory, stateHistory);
+    }
+
+    [Fact]
+    public void ReviewLog_ElapsedDays_ShouldBeZero_ForNewCards_WhenScheduledDaysIsSet()
+    {
+        var fsrs = new FSRS();
+        var card = new Card();
+        card.ScheduledDays = 42;
+
+        var log = fsrs.Repeat(card, new DateTime(2023, 11, 10, 23, 20, 47, 297));
+        var againReviewLog = log[Rating.Again].ReviewLog;
+
+        Assert.Equal(0, againReviewLog.ElapsedDays);
+    }
+
+    [Fact]
+    public void ReviewLog_ScheduledDays_ShouldBeZero_ForAgainRatings_IrregardlessOfElapsedDaysSinceLastReview()
+    {
+        var fsrs = new FSRS();
+        var card = new Card();
+        card.State = State.Learning;
+        card.LastReview = new DateTime(2023, 11, 6, 23, 20, 47, 297);
+
+        var log = fsrs.Repeat(card, new DateTime(2023, 11, 10, 23, 20, 47, 297));
+        var againReviewLog = log[Rating.Again].ReviewLog;
+
+        Assert.Equal(0, againReviewLog.ScheduledDays);
+    }
+}
--- TanaInt.Domain/Srs/Fsrs/Fsrs.cs
+++ TanaInt.Domain/Srs/Fsrs/Fsrs.cs
@@ -1,3 +1,4 @@
+// Credits goes to https://github.com/open-spaced-repetition/fsrs.js
 namespace TanaInt.Domain.Srs.Fsrs;
 
 using System;
@@ -21,265 +22,278 @@ public enum Rating
 
 public class ReviewLog
 {
-    public Rating rating;
-    public int scheduled_days;
-    public int elapsed_days;
-    public DateTime review;
-    public State state;
+    public Rating Rating { get; set; }
+    public int ScheduledDays { get; set; }
+    public int ElapsedDays { get; set; }
+    public DateTime Review { get; set; }
+    public State State { get; set; }
 
-    public ReviewLog(Rating rating, int scheduled_days, int elapsed_days, DateTime review, State state)
+    public ReviewLog(Rating rating, int scheduledDays, int elapsedDays, DateTime review, State state)
     {
-        this.rating = rating;
-        this.elapsed_days = elapsed_days;
-        this.scheduled_days = scheduled_days;
-        this.review = review;
-        this.state = state;
+        Rating = rating;
+        ElapsedDays = elapsedDays;
+        ScheduledDays = scheduledDays;
+        Review = review;
+        State = state;
     }
 }
 
 public class Card
 {
-    public DateTime due;
-    public double stability;
-    public double difficulty;
-    public int elapsed_days;
-    public int scheduled_days;
-    public int reps;
-    public int lapses;
-    public State state;
-    public DateTime last_review;
+    public DateTime Due { get; set; }
+    public double Stability { get; set; }
+    public double Difficulty { get; set; }
+    public int ElapsedDays { get; set; }
+    public int ScheduledDays { get; set; }
+    public int Reps { get; set; }
+    public int Lapses { get; set; }
+    public State State { get; set; }
+    public DateTime LastReview { get; set; }
 
     public Card()
     {
-        this.due = DateTime.Now;
-        this.stability = 0;
-        this.difficulty = 0;
-        this.elapsed_days = 0;
-        this.scheduled_days = 0;
-        this.reps = 0;
-        this.lapses = 0;
-        this.state = State.New;
-        this.last_review = DateTime.Now;
+        Due = DateTime.Now;
+        Stability = 0;
+        Difficulty = 0;
+        ElapsedDays = 0;
+        ScheduledDays = 0;
+        Reps = 0;
+        Lapses = 0;
+        State = State.New;
+        LastReview = DateTime.Now;
+    }
+
+    public Card(Card card)
+    {
+        Due = card.Due;
+        Stability = card.Stability;
+        Difficulty = card.Difficulty;
+        ElapsedDays = card.ElapsedDays;
+        ScheduledDays = card.ScheduledDays;
+        Reps = card.Reps;
+        Lapses = card.Lapses;
+        State = card.State;
+        LastReview = card.LastReview;
     }
 }
 
 public class SchedulingInfo
 {
-    public Card card;
-    public ReviewLog review_log;
+    public Card Card { get; set; }
+    public ReviewLog ReviewLog { get; set; }
 
-    public SchedulingInfo(Card card, ReviewLog review_log)
+    public SchedulingInfo(Card card, ReviewLog reviewLog)
     {
-        this.card = card;
-        this.review_log = review_log;
+        Card = card;
+        ReviewLog = reviewLog;
     }
 }
 
 public class SchedulingCards
 {
-    public Card again;
-    public Card hard;
-    public Card good;
-    public Card easy;
+    public Card Again { get; set; }
+    public Card Hard { get; set; }
+    public Card Good { get; set; }
+    public Card Easy { get; set; }
 
     public SchedulingCards(Card card)
     {
-        this.again = new Card();
-        this.hard = new Card();
-        this.good = new Card();
-        this.easy = new Card();
+        Again = new Card(card);
+        Hard = new Card(card);
+        Good = new Card(card);
+        Easy = new Card(card);
     }
 
-    public void update_state(State state)
+    public void UpdateState(State state)
     {
         if (state == State.New)
         {
-            this.again.state = State.Learning;
-            this.hard.state = State.Learning;
-            this.good.state = State.Learning;
-            this.easy.state = State.Review;
+            Again.State = State.Learning;
+            Hard.State = State.Learning;
+            Good.State = State.Learning;
+            Easy.State = State.Review;
         }
         else if (state == State.Learning || state == State.Relearning)
         {
-            this.again.state = state;
-            this.hard.state = state;
-            this.good.state = State.Review;
-            this.easy.state = State.Review;
+            Again.State = state;
+            Hard.State = state;
+            Good.State = State.Review;
+            Easy.State = State.Review;
         }
         else if (state == State.Review)
         {
-            this.again.state = State.Relearning;
-            this.hard.state = State.Review;
-            this.good.state = State.Review;
-            this.easy.state = State.Review;
-            this.again.lapses += 1;
+            Again.State = State.Relearning;
+            Hard.State = State.Review;
+            Good.State = State.Review;
+            Easy.State = State.Review;
+            Again.Lapses += 1;
         }
     }
 
-    public void schedule(DateTime now, int hard_interval, int good_interval, int easy_interval)
+    public void Schedule(DateTime now, int hardInterval, int goodInterval, int easyInterval)
     {
-        this.again.scheduled_days = 0;
-        this.hard.scheduled_days = hard_interval;
-        this.good.scheduled_days = good_interval;
-        this.easy.scheduled_days = easy_interval;
+        Again.ScheduledDays = 0;
+        Hard.ScheduledDays = hardInterval;
+        Good.ScheduledDays = goodInterval;
+        Easy.ScheduledDays = easyInterval;
 
-        this.again.due = now.AddMinutes(5);
-        if (hard_interval > 0)
+        Again.Due = now.AddMinutes(5);
+        if (hardInterval > 0)
         {
-            this.hard.due = now.AddDays(hard_interval);
+            Hard.Due = now.AddDays(hardInterval);
         }
         else
         {
-            this.hard.due = now.AddMinutes(10);
+            Hard.Due = now.AddMinutes(10);
         }
-        this.good.due = now.AddDays(good_interval);
-        this.easy.due = now.AddDays(easy_interval);
+        Good.Due = now.AddDays(goodInterval);
+        Easy.Due = now.AddDays(easyInterval);
     }
 
-    public Dictionary<Rating, SchedulingInfo> record_log(Card card, DateTime now)
+    public Dictionary<Rating, SchedulingInfo> RecordLog(Card card, DateTime now)
     {
         return new Dictionary<Rating, SchedulingInfo>
         {
-            [Rating.Again] = new SchedulingInfo(this.again, new ReviewLog(Rating.Again, this.again.scheduled_days, card.elapsed_days, now, card.state)),
-            [Rating.Hard] = new SchedulingInfo(this.hard, new ReviewLog(Rating.Hard, this.hard.scheduled_days, card.elapsed_days, now, card.state)),
-            [Rating.Good] = new SchedulingInfo(this.good, new ReviewLog(Rating.Good, this.good.scheduled_days, card.elapsed_days, now, card.state)),
-            [Rating.Easy] = new SchedulingInfo(this.easy, new ReviewLog(Rating.Easy, this.easy.scheduled_days, card.elapsed_days, now, card.state)),
+            [Rating.Again] = new SchedulingInfo(Again, new ReviewLog(Rating.Again, Again.ScheduledDays, card.ElapsedDays, now, card.State)),
+            [Rating.Hard] = new SchedulingInfo(Hard, new ReviewLog(Rating.Hard, Hard.ScheduledDays, card.ElapsedDays, now, card.State)),
+            [Rating.Good] = new SchedulingInfo(Good, new ReviewLog(Rating.Good, Good.ScheduledDays, card.ElapsedDays, now, card.State)),
+            [Rating.Easy] = new SchedulingInfo(Easy, new ReviewLog(Rating.Easy, Easy.ScheduledDays, card.ElapsedDays, now, card.State)),
         };
     }
 }
 
 public class Params
 {
-    public double request_retention;
-    public int maximum_interval;
-    public double[] w;
+    public double RequestRetention { get; set; }
+    public int MaximumInterval { get; set; }
+    public double[] W { get; set; }
 
     public Params()
     {
-        this.request_retention = 0.9;
-        this.maximum_interval = 36500;
-        this.w = new double[] { 0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34, 1.26, 0.29, 2.61 };
+        RequestRetention = 0.9;
+        MaximumInterval = 36500;
+        W = new double[] { 0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34, 1.26, 0.29, 2.61 };
     }
 }
 
 public class FSRS
 {
-    public Params p;
+    public Params P { get; set; }
 
     public FSRS()
     {
-        this.p = new Params();
+        P = new Params();
     }
 
-    public Dictionary<Rating, SchedulingInfo> repeat(Card card, DateTime now)
+    public Dictionary<Rating, SchedulingInfo> Repeat(Card card, DateTime now)
     {
-        card = new Card();
-        if (card.state == State.New)
+        var clonedCard = new Card(card);
+        if (clonedCard.State == State.New)
         {
-            card.elapsed_days = 0;
+            clonedCard.ElapsedDays = 0;
         }
         else
         {
-            card.elapsed_days = (int)(now - card.last_review).TotalDays;
+            clonedCard.ElapsedDays = (int)(now - clonedCard.LastReview).TotalDays;
         }
-        card.last_review = now;
-        card.reps += 1;
-        SchedulingCards s = new SchedulingCards(card);
-        s.update_state(card.state);
-        if (card.state == State.New)
+        clonedCard.LastReview = now;
+        clonedCard.Reps += 1;
+        SchedulingCards s = new SchedulingCards(clonedCard);
+        s.UpdateState(clonedCard.State);
+        if (clonedCard.State == State.New)
         {
-            this.init_ds(s);
-            s.again.due = now.AddMinutes(1);
-            s.hard.due = now.AddMinutes(5);
-            s.good.due = now.AddMinutes(10);
-            int easy_interval = this.next_interval(s.easy.stability);
-            s.easy.scheduled_days = easy_interval;
-            s.easy.due = now.AddDays(easy_interval);
+            InitDs(s);
+            s.Again.Due = now.AddMinutes(1);
+            s.Hard.Due = now.AddMinutes(5);
+            s.Good.Due = now.AddMinutes(10);
+            int easyInterval = NextInterval(s.Easy.Stability);
+            s.Easy.ScheduledDays = easyInterval;
+            s.Easy.Due = now.AddDays(easyInterval);
         }
-        else if (card.state == State.Learning || card.state == State.Relearning)
+        else if (clonedCard.State == State.Learning || clonedCard.State == State.Relearning)
         {
-            int hard_interval = 0;
-            int good_interval = this.next_interval(s.good.stability);
-            int easy_interval = Math.Max(this.next_interval(s.easy.stability), good_interval + 1);
-            s.schedule(now, hard_interval, good_interval, easy_interval);
+            int hardInterval = 0;
+            int goodInterval = NextInterval(s.Good.Stability);
+            int easyInterval = Math.Max(NextInterval(s.Easy.Stability), goodInterval + 1);
+            s.Schedule(now, hardInterval, goodInterval, easyInterval);
         }
-        else if (card.state == State.Review)
+        else if (clonedCard.State == State.Review)
         {
-            int interval = card.elapsed_days;
-            double last_d = card.difficulty;
-            double last_s = card.stability;
-            double retrievability = Math.Pow(1 + interval / (9 * last_s), -1);
-            this.next_ds(s, last_d, last_s, retrievability);
-            int hard_interval = this.next_interval(s.hard.stability);
-            int good_interval = this.next_interval(s.good.stability);
-            hard_interval = Math.Min(hard_interval, good_interval);
-            good_interval = Math.Max(good_interval, hard_interval + 1);
-            int easy_interval = Math.Max(this.next_interval(s.easy.stability), good_interval + 1);
-            s.schedule(now, hard_interval, good_interval, easy_interval);
+            int interval = clonedCard.ElapsedDays;
+            double lastD = clonedCard.Difficulty;
+            double lastS = clonedCard.Stability;
+            double retrievability = Math.Pow(1 + interval / (9 * lastS), -1);
+            NextDs(s, lastD, lastS, retrievability);
+            int hardInterval = NextInterval(s.Hard.Stability);
+            int goodInterval = NextInterval(s.Good.Stability);
+            hardInterval = Math.Min(hardInterval, goodInterval);
+            goodInterval = Math.Max(goodInterval, hardInterval + 1);
+            int easyInterval = Math.Max(NextInterval(s.Easy.Stability), goodInterval + 1);
+            s.Schedule(now, hardInterval, goodInterval, easyInterval);
         }
-        return s.record_log(card, now);
+        return s.RecordLog(clonedCard, now);
     }
 
-    public void init_ds(SchedulingCards s)
+    private void InitDs(SchedulingCards s)
     {
-        s.again.difficulty = this.init_difficulty(Rating.Again);
-        s.again.stability = this.init_stability(Rating.Again);
-        s.hard.difficulty = this.init_difficulty(Rating.Hard);
-        s.hard.stability = this.init_stability(Rating.Hard);
-        s.good.difficulty = this.init_difficulty(Rating.Good);
-        s.good.stability = this.init_stability(Rating.Good);
-        s.easy.difficulty = this.init_difficulty(Rating.Easy);
-        s.easy.stability = this.init_stability(Rating.Easy);
+        s.Again.Difficulty = InitDifficulty(Rating.Again);
+        s.Again.Stability = InitStability(Rating.Again);
+        s.Hard.Difficulty = InitDifficulty(Rating.Hard);
+        s.Hard.Stability = InitStability(Rating.Hard);
+        s.Good.Difficulty = InitDifficulty(Rating.Good);
+        s.Good.Stability = InitStability(Rating.Good);
+        s.Easy.Difficulty = InitDifficulty(Rating.Easy);
+        s.Easy.Stability = InitStability(Rating.Easy);
     }
 
-    public void next_ds(SchedulingCards s, double last_d, double last_s, double retrievability)
+    private void NextDs(SchedulingCards s, double lastD, double lastS, double retrievability)
     {
-        s.again.difficulty = this.next_difficulty(last_d, Rating.Again);
-        s.again.stability = this.next_forget_stability(last_d, last_s, retrievability);
-        s.hard.difficulty = this.next_difficulty(last_d, Rating.Hard);
-        s.hard.stability = this.next_recall_stability(last_d, last_s, retrievability, Rating.Hard);
-        s.good.difficulty = this.next_difficulty(last_d, Rating.Good);
-        s.good.stability = this.next_recall_stability(last_d, last_s, retrievability, Rating.Good);
-        s.easy.difficulty = this.next_difficulty(last_d, Rating.Easy);
-        s.easy.stability = this.next_recall_stability(last_d, last_s, retrievability, Rating.Easy);
+        s.Again.Difficulty = NextDifficulty(lastD, Rating.Again);
+        s.Again.Stability = NextForgetStability(lastD, lastS, retrievability);
+        s.Hard.Difficulty = NextDifficulty(lastD, Rating.Hard);
+        s.Hard.Stability = NextRecallStability(lastD, lastS, retrievability, Rating.Hard);
+        s.Good.Difficulty = NextDifficulty(lastD, Rating.Good);
+        s.Good.Stability = NextRecallStability(lastD, lastS, retrievability, Rating.Good);
+        s.Easy.Difficulty = NextDifficulty(lastD, Rating.Easy);
+        s.Easy.Stability = NextRecallStability(lastD, lastS, retrievability, Rating.Easy);
     }
 
-    public double init_stability(Rating r)
+    private double InitStability(Rating r)
     {
-        return Math.Max(this.p.w[(int)r - 1], 0.1);
+        return Math.Max(P.W[(int)r - 1], 0.1);
     }
 
-    public double init_difficulty(Rating r)
+    private double InitDifficulty(Rating r)
     {
-        return Math.Min(Math.Max(this.p.w[4] - this.p.w[5] * ((int)r - 3), 1), 10);
+        return Math.Min(Math.Max(P.W[4] - P.W[5] * ((int)r - 3), 1), 10);
     }
 
-    public int next_interval(double s)
+    private int NextInterval(double s)
     {
-        double interval = s * 9 * (1 / this.p.request_retention - 1);
-        return Math.Min(Math.Max((int)Math.Round(interval), 1), this.p.maximum_interval);
+        double interval = s * 9 * (1 / P.RequestRetention - 1);
+        return Math.Min(Math.Max((int)Math.Round(interval), 1), P.MaximumInterval);
     }
 
-    public double next_difficulty(double d, Rating r)
+    private double NextDifficulty(double d, Rating r)
     {
-        double next_d = d - this.p.w[6] * ((int)r - 3);
-        return Math.Min(Math.Max(this.mean_reversion(this.p.w[4], next_d), 1), 10);
+        double nextD = d - P.W[6] * ((int)r - 3);
+        return Math.Min(Math.Max(MeanReversion(P.W[4], nextD), 1), 10);
     }
 
-    public double mean_reversion(double init, double current)
+    private double MeanReversion(double init, double current)
     {
-        return this.p.w[7] * init + (1 - this.p.w[7]) * current;
+        return P.W[7] * init + (1 - P.W[7]) * current;
     }
 
-    public double next_recall_stability(double d, double s, double r, Rating rating)
+    private double NextRecallStability(double d, double s, double r, Rating rating)
     {
-        double hard_penalty = rating == Rating.Hard ? this.p.w[15] : 1;
-        double easy_bonus = rating == Rating.Easy ? this.p.w[16] : 1;
-        return s * (1 + Math.Exp(this.p.w[8]) * (11 - d) * Math.Pow(s, -this.p.w[9]) * (Math.Exp((1 - r) * this.p.w[10]) - 1) * hard_penalty * easy_bonus);
+        double hardPenalty = rating == Rating.Hard ? P.W[15] : 1;
+        double easyBonus = rating == Rating.Easy ? P.W[16] : 1;
+        return s * (1 + Math.Exp(P.W[8]) * (11 - d) * Math.Pow(s, -P.W[9]) * (Math.Exp((1 - r) * P.W[10]) - 1) * hardPenalty * easyBonus);
     }
 
-    public double next_forget_stability(double d, double s, double r)
+    private double NextForgetStability(double d, double s, double r)
     {
-        return this.p.w[11] * Math.Pow(d, -this.p.w[12]) * (Math.Pow(s + 1, this.p.w[13]) - 1) * Math.Exp((1 - r) * this.p.w[14]);
+        return P.W[11] * Math.Pow(d, -P.W[12]) * (Math.Pow(s + 1, P.W[13]) - 1) * Math.Exp((1 - r) * P.W[14]);
     }
 }
