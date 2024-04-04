bb086d302e5b49cf01d9a8905add9a82d2fc0a19
popemkt <hoangng71299@gmail.com>
2024-04-05 00:57:59 +0700

    Fix

--- TanaInt.Domain/Srs/Fsrs/FsrsDto.cs
+++ TanaInt.Domain/Srs/Fsrs/FsrsDto.cs
@@ -6,8 +6,12 @@ public class FsrsDto
 {
     [JsonPropertyName("fsrsString")] public string? FsrsString { get; set; }
     [JsonPropertyName("fsrsParameters")] public double[]? FsrsParameters { get; set; }
-    [JsonPropertyName("rating")] public Rating Rating { get; set; }
-    [JsonIgnore] public Card Card { get; set; }
+
+    [JsonPropertyName("rating")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Rating Rating { get; set; }
+
+    [JsonIgnore] public Card? Card { get; set; }
 
     public FsrsDto ParseInput()
     {
--- TanaInt.Infrastructure/Services/FsrsService.cs
+++ TanaInt.Infrastructure/Services/FsrsService.cs
@@ -7,7 +7,7 @@ public class FsrsService : IFsrsService
     public Card Repeat(FsrsDto dto, DateTime now)
     {
         var fsrs = new Fsrs();
-        var result = fsrs.Repeat(dto.Card, DateTime.Now)[dto.Rating];
+        var result = fsrs.Repeat(dto.Card ?? new Card(), DateTime.Now)[dto.Rating];
         return result.Card;
     }
 }
