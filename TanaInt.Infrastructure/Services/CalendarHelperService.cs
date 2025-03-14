0564c2fddce47ce7d0b00d69cf71b02dd2b9f12b
Hoang Nguyen <hoangng71299@gmail.com>
2025-03-14 23:34:25 +0700

    Fix rrule

--- TanaInt.Infrastructure/Services/CalendarHelperService.cs
+++ TanaInt.Infrastructure/Services/CalendarHelperService.cs
@@ -1,3 +1,4 @@
+using Ical.Net;
 using Ical.Net.DataTypes;
 using Ical.Net.Evaluation;
 using TanaInt.Domain.Calendar;
@@ -24,6 +25,41 @@ public class CalendarRecurrenceService : ICalendarRecurrenceService
             IncrementDate(ref nextDate, pattern, interval);
             return nextDate;
         }
+        
+        //Taken directly from Ical.Net source, with modified case of FrequencyType.Weekly
+        private void IncrementDate(ref DateTime dt, RecurrencePattern pattern, int interval)
+        {
+            if (interval == 0)
+                throw new Exception("Cannot evaluate with an interval of zero.  Please use an interval other than zero.");
+            DateTime dt1 = dt;
+            switch (pattern.Frequency)
+            {
+                case FrequencyType.Secondly:
+                    dt = dt1.AddSeconds(interval);
+                    break;
+                case FrequencyType.Minutely:
+                    dt = dt1.AddMinutes(interval);
+                    break;
+                case FrequencyType.Hourly:
+                    dt = dt1.AddHours(interval);
+                    break;
+                case FrequencyType.Daily:
+                    dt = dt1.AddDays(interval);
+                    break;
+                case FrequencyType.Weekly:
+                    dt = dt1.AddDays(interval * 7);
+                    break;
+                case FrequencyType.Monthly:
+                    dt = dt1.AddDays(-dt1.Day + 1).AddMonths(interval);
+                    break;
+                case FrequencyType.Yearly:
+                    dt = dt1.AddDays(-dt1.DayOfYear + 1).AddYears(interval);
+                    break;
+                default:
+                    throw new Exception("FrequencyType.NONE cannot be evaluated. Please specify a FrequencyType before evaluating the recurrence.");
+            }
+        }
+
     }
 
     public RecurrencePattern ParseRRule(string rrule)
